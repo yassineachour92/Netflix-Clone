99cbb5ee589b4e19320ef81aca84e150445a71fa
Yassine <yachour92@gmail.com>
2024-06-10 16:14:02 +0200

    feat: ajouter les constantes et les les APIs

---
+++ src/constant.js
@@ -0,0 +1,2 @@
+export const baseURL = "https://api.themoviedb.org/3/";
+export const imgUrl="https://image.tmdb.org/t/p/original/"
--- src/requests.js
+++ src/requests.js
@@ -1,8 +1,8 @@
 const API_KEY = "83294763349fd4b886aa0950487766ea";
 
 const requests = {
-  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchNetflixOriginals: `/discover/tv?api_key=${API_KEY}&with_networks=213`,
+  fetchTrending: `/trending/all/week?api_key=${API_KEY}&language=en-US`,
   fetchTopRated: `/movie/top_rated?api_key=${API_KEY}&language=/-US`,
   fetchActionMovies: `/discover/movie?api_key=${API_KEY}&with_genres=28`,
   fetchComedyMovies: `/discover/movie?api_key=${API_KEY}&with_genres=35`,
