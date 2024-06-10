1a20d928afe1e2982242b8ffb7a20d0d085394ce
Yassine <yachour92@gmail.com>
2024-06-10 16:14:53 +0200

    feat(app): afficher les blocks des films par categorie

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import React  from 'react';
+import requests from './requests.js'
+import Row from './Row/Row.tsx';
+
 import './App.css';
+
+const generateRows=(requests)=> {
+  return Object.keys(requests).map(key => {
+    const title = key.replace('fetch', '').replace(/([A-Z])/g, ' $1').trim();
+    return <Row key={key} title={title} fetchUrl={requests[key]} isLargeRow={title==="Netflix Originals"}/>;
+  });
+}
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
+      {generateRows(requests)}
+
+
     </div>
   );
 }
